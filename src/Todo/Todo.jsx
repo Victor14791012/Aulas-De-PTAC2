46e7f74fe27ef6f99f4dd93bcba9ed25c21d81eb
Victor14791012 <victor.carvalhok23@gmail.com>
2023-09-05 15:50:07 -0400

    atualizações
    aaaa

--- src/Todo/Todo.jsx
+++ src/Todo/Todo.jsx
@@ -1,51 +1,80 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './todo.css';
 
 export default function ToDo() {
-    const [atividade, setAtividade]= useState("");
+    const [atividade, setAtividade] = useState("");
+    const [descricao, setDescricao] = useState("");
+    const [forca, setForca] = useState("");
+    const [imagem, setImagem] = useState("");
     const [Lista, setLista] = useState([]);
-    const [id, setId]= useState(1);
-    const salvar =(e)=>{
+    const [id, setId] = useState(1);
+
+    const salvar = (e) => {
         e.preventDefault();
         setLista([...Lista, {
-
-            atividade: atividade ,
-            id:id
-            
-        }])
-        setId(id + 1)
+            atividade: atividade,
+            descricao: descricao,
+            forca: forca,
+            imagem: imagem,
+            id: id
+        }]);
+        setId(id + 1);
         setAtividade("");
+        setDescricao("");
+        setForca("");
+        setImagem("");
     };
 
-    const deletar =(e)=>{
-        setLista([...Lista, {
-
-            atividade: atividade ,
-            id:id
-            
-        }]);
-       
-       
+    const deletar = (idToDelete) => {
+        const novaLista = Lista.filter((ativ) => ativ.id !== idToDelete);
+        setLista(novaLista);
     };
 
     return (   
-        <div>
-            <Link to="/home">home</Link>   
-            <h1>Lista de Atividades</h1>  
-            
-
-             <form onSubmit={salvar}>
-               <input type ="text" value={atividade} onChange={(e) => setAtividade(e.target.value)} />
-               <button>ADD</button>
-               <button onSubmit={deletar}>DEL</button>
-             </form>
-
-            { Lista.map((ativ)=> 
-                <div key={ativ.id}>
-                      <p> {ativ.atividade} </p>
-
-                </div>  )}
+        <div className="container" id="transparente">
+            <Link to="/home">Home</Link> 
+            <div className="bg-white rounded-2 p-2">
+                <h1>Portal Geek</h1>  
+                <h6>Adicione seus personagens favoritos</h6>
                 
-        </div>
+                <form onSubmit={salvar}>
+                    <div className="mb-3">
+                        <label htmlFor="atividade" className="form-label">Nome (personagem)</label>
+                        <input type="text" className="form-control" id="atividade" value={atividade} onChange={(e) => setAtividade(e.target.value)} />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="descricao" className="form-label">Descrição (personagem)</label>
+                        <input type="text" className="form-control" id="descricao" value={descricao} onChange={(e) => setDescricao(e.target.value)} />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="forca" className="form-label">Força (personagem)</label>
+                        <input type="text" className="form-control" id="forca" value={forca} onChange={(e) => setForca(e.target.value)} />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="imagem" className="form-label">Imagem (PNG ou JPG)</label>
+                        <input type="file" className="form-control" id="imagem" accept=".png, .jpg, .jpeg" onChange={(e) => setImagem(e.target.files[0])} />
+                    </div>
+                    <button className="btn btn-primary">ADD</button>
+                </form>
+            </div>
+
+            <div className="row m-2">
+                {Lista.map((ativ) => (
+                    <div key={ativ.id} className="col-md-4">
+                        <div className="card mb-4" id="card-meu">
+                            <img src={ativ.imagem && URL.createObjectURL(ativ.imagem)} className="card-img-top" alt="Imagem" />
+                            <div className="card-body">
+                                <h5 className="card-title">{ativ.atividade}</h5>
+                                <p className="card-text">Descrição: {ativ.descricao}</p>
+                                <p className="card-text">Força: {ativ.forca}</p> {/* Correção aqui */}
+                                <button className="btn btn-danger" onClick={() => deletar(ativ.id)}>DEL</button>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div> 
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 
+
+
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from "./Home/Home.jsx";
 import Todo from "./Todo/Todo.jsx";
