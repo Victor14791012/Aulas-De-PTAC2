575d8822c95f1c7911db0b4214167907b6c66b4b
Victor14791012 <victor.carvalhok23@gmail.com>
2023-09-21 23:50:00 -0400

    Update Todo.jsx
    versão final

--- src/Todo/Todo.jsx
+++ src/Todo/Todo.jsx
@@ -2,81 +2,56 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-
-
-
 import './todo.css';
 
 export default function ToDo() {
+    // Estados para armazenar informações do formulário e da lista de atividades
     const [atividade, setAtividade] = useState("");
     const [descricao, setDescricao] = useState("");
     const [forca, setForca] = useState("");
     const [imagem, setImagem] = useState("");
     const [Lista, setLista] = useState([]);
-    const [id, setId] = useState(1);
-    const [editandoId, setEditandoId] = useState(null);
+    const [id, setId] = useState(1); // Contador para atribuir IDs únicos
 
+    // Função para adicionar uma atividade
     const salvar = (e) => {
         e.preventDefault();
-
-       /* if (!atividade || !descricao || !forca || !imagem) {
+ /*
+        if (!atividade || !descricao || !forca || !imagem) {
             alert("Por favor, preencha todos os campos antes de adicionar.");
             return;
         } */
 
-        if (editandoId === null) {
-            // Adicionar um novo objeto
-            setLista([...Lista, {
-                atividade: atividade,
-                descricao: descricao,
-                forca: forca,
-                imagem: imagem,
-                id: id
-            }]);
-            setId(id + 1);
-        } else {
-            // Editar um objeto existente
-            const novaLista = Lista.map((ativ) =>
-                ativ.id === editandoId
-                    ? {
-                          ...ativ,
-                          atividade: atividade,
-                          descricao: descricao,
-                          forca: forca,
-                          imagem: imagem,
-                      }
-                    : ativ
-            );
-            setLista(novaLista);
-            setEditandoId(null);
-        }
+        // Adicionar um novo objeto à lista
+        setLista([...Lista, {
+            atividade: atividade,
+            descricao: descricao,
+            forca: forca,
+            imagem: imagem,
+            id: id
+        }]);
+        setId(id + 1); // Incrementar o contador de IDs
 
+        // Limpar os campos do formulário
         setAtividade("");
         setDescricao("");
         setForca("");
         setImagem("");
     };
 
-    const editar = (idEditar) => {
-        const objetoParaEditar = Lista.find((ativ) => ativ.id === idEditar);
-        setEditandoId(idEditar);
-        setAtividade(objetoParaEditar.atividade);
-        setDescricao(objetoParaEditar.descricao);
-        setForca(objetoParaEditar.forca);
-        setImagem(objetoParaEditar.imagem);
-    };
-
+    // Função para excluir uma atividade da lista
     const deletar = (idToDelete) => {
         const novaLista = Lista.filter((ativ) => ativ.id !== idToDelete);
         setLista(novaLista);
     };
 
+    // Função para excluir todas as atividades da lista
     const deletarTodos = () => {
         setLista([]);
     };
 
     return (   
-        <div className="container " id="transparente">
+        <div className="container" id="transparente">
             <Link to="/home">Home</Link> 
             <div className="bg-dark text-white rounded-2 p-2">
                 <h1>Portal Geek</h1>  
@@ -99,14 +74,9 @@ export default function ToDo() {
                         <label htmlFor="imagem" className="form-label">Imagem (PNG ou JPG)</label>
                         <input type="file" className="form-control" id="imagem" accept=".png, .jpg, .jpeg" onChange={(e) => setImagem(e.target.files[0])} />
                     </div>
-                    <button className="btn btn-primary">{editandoId === null ? 'ADD' : 'Salvar Edição'}</button>
+                    <button className="btn btn-primary">ADD</button>
                     <button className="btn btn-danger mx-2" onClick={deletarTodos}> DEL <i className="bi  bi-trash"></i> </button>
-                   
-                    
- 
-
                 </form>
-                
             </div>
 
             <div className="row m-2">
@@ -121,10 +91,6 @@ export default function ToDo() {
                                 <button className="btn btn-danger butao float-right" onClick={() => deletar(ativ.id)}>
                                     <i className="bi bi-trash"></i> DEL
                                 </button>
-                                <button className="btn btn-info butao float-right mx-2" onClick={() => editar(ativ.id)}>
-                                    <i className="bi bi-pencil"></i> Editar
-                                </button>
-                                
                             </div>
                         </div>
                     </div>
