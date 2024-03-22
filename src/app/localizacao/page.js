09f3fb9be2f93adae8c2fb0fd9c505257db78b97
Victor14791012 <victor.carvalhok23@gmail.com>
2024-03-22 13:55:08 -0400

    update atual

---
+++ src/app/localizacao/page.js
@@ -0,0 +1,10 @@
+import Map from "../componentes/Map";
+
+export default function localizacao(){
+    return( 
+        <div className="text-center py-5 mb-5">
+            <h3 className="pb-3">Página de Localização!;</h3>
+            <Map/>
+        </div>
+    );
+}
